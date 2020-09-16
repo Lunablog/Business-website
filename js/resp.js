ebfdb05cd515e452b30882145fb599fa55efd480
Lunatic <lunayadav123@gmail.com>
2020-09-16 12:45:37 +0545

    Initial commit

---
+++ js/resp.js
@@ -0,0 +1,16 @@
+burger=document.querySelector('.burger')
+navBar=document.querySelector('.navbar')
+navList=document.querySelector('.nav-list')
+rightNav=document.querySelector('.rightnav')
+
+
+burger.addEventListener('click',()=>{
+    rightNav.classList.toggle('v-class-resp')
+    navList.classList.toggle('v-class-resp')
+    navBar.classList.toggle('h-class-resp')
+    
+
+
+
+
+})
